e0b2d09476707fed7f457b453b853f674b61d51b
Thomas Traub <thomastraub2000@yahoo.de>
2011-09-03 18:08:50 +0200

    Updated to v2.0.3

--- profiler_lang.php
+++ profiler_lang.php
@@ -9,6 +9,7 @@ $lang['profiler_post_data']		= 'POST-DATEN';
 $lang['profiler_uri_string']	= 'URI Zeichenkette';
 $lang['profiler_memory_usage']	= 'Speicher Nutzung';
 $lang['profiler_config']		= 'CONFIG VARIABLEN';
+$lang['profiler_session_data']	= 'SESSION-DATEN';
 $lang['profiler_headers']		= 'HTTP HEADERS';
 $lang['profiler_no_db']			= 'Der Datenbanktreiber ist nicht geladen';
 $lang['profiler_no_queries']	= 'Keine Abfragen durchgef&uuml;hrt';
@@ -17,6 +18,8 @@ $lang['profiler_no_get']		= 'Keine GET-Daten vorhanden';
 $lang['profiler_no_uri']		= 'Keine URI vorhanden';
 $lang['profiler_no_memory']		= 'Speichernutzung unbekannt';
 $lang['profiler_no_profiles']	= 'Keine Profiler Daten - alle Sektionen sind deaktivert.';
+$lang['profiler_section_hide']	= 'Verbergen';
+$lang['profiler_section_show']	= 'Zeigen';
 
 /* End of file profiler_lang.php */
 /* Location: ./system/language/german/profiler_lang.php */
