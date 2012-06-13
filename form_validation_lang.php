f62123430e4079d83d29e3cb83ef54475d54d848
Thomas Traub <thomastraub2000@yahoo.de>
2012-06-13 22:24:50 +0200

    Add license comment blocks, update form_validation
    - synch to CodeIgniter develop branch

--- form_validation_lang.php
+++ form_validation_lang.php
@@ -1,28 +1,55 @@
 <?php
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP 5.2.4 or newer
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the Open Software License version 3.0
+ *
+ * This source file is subject to the Open Software License (OSL 3.0) that is
+ * bundled with this package in the files license.txt / license.rst.  It is
+ * also available through the world wide web at this URL:
+ * http://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to obtain it
+ * through the world wide web, please send an email to
+ * licensing@ellislab.com so we can send you a copy immediately.
+ *
+ * @package		CodeIgniter
+ * @author		EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2012, EllisLab, Inc. (http://ellislab.com/)
+ * @license		http://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * @link		http://codeigniter.com
+ * @since		Version 1.0
+ * @filesource
+ */
 
-$lang['required']			= "Das Feld %s ist ein Pflichtfeld.";
-$lang['isset']				= "Das Feld %s muss einen Wert enthalten.";
-$lang['valid_email']		= "Das Feld %s muss eine g&uuml;ltige E-Mail-Adresse enthalten.";
-$lang['valid_emails']		= "Das Feld %s kann nur g&uuml;ltige E-Mail-Adressen enthalten.";
-$lang['valid_url']			= "Das Feld %s muss eine g&uuml;ltige Web-Adresse (URL) enthalten.";
-$lang['valid_ip']			= "Das Feld %s muss eine g&uuml;ltige IP enthalten.";
-$lang['min_length']			= "Das Feld %s muss mindestens %s Zeichen lang sein.";
-$lang['max_length']			= "Das Feld %s darf nicht l&auml;nger als %s Zeichen lang sein.";
-$lang['exact_length']		= "Das Feld %s muss genau %s Zeichen lang sein.";
-$lang['alpha']				= "Das Feld %s darf nur Buchstaben enthalten.";
-$lang['alpha_numeric']		= "Das Feld %s darf nur Buchstaben und/oder Zahlen enthalten.";
-$lang['alpha_dash']			= "Das Feld %s darf nur Buchstaben, Zahlen, Bindestriche und Unterstriche enthalten.";
-$lang['numeric']			= "Das Feld %s darf nur Zahlen enthalten.";
-$lang['is_numeric']			= "Das Feld %s darf nur zahlen&auml;hnliche Zeichen enthalten.";
-$lang['integer']			= "Das Feld %s darf nur ganze Zahlen enthalten.";
-$lang['regex_match']		= "Das Feld %s ist nicht im richtigen Format.";
-$lang['matches']			= "Das Feld %s muss mit dem Feld %s &uuml;bereinstimmen.";
-$lang['is_unique'] 			= "Das Feld %s muss einen einzigartigen Wert enthalten.";
-$lang['is_natural']			= "Das Feld %s darf nur positive Zahlen enthalten.";
-$lang['is_natural_no_zero']	= "Das Feld %s darf nur Zahlen gr&ouml;sser als Null enthalten.";
-$lang['decimal']			= "Das Feld %s muss eine Dezimalzahl enthalten.";
-$lang['less_than']			= "Das Feld %s muss eine Zahl kleiner als %s enthalten.";
-$lang['greater_than']		= "Das Feld %s muss eine Zahl gr&ouml;sser als %s enthalten.";
+$lang['required']				= "Das Feld %s ist ein Pflichtfeld.";
+$lang['isset']					= "Das Feld %s muss einen Wert enthalten.";
+$lang['valid_email']			= "Das Feld %s muss eine g&uuml;ltige E-Mail-Adresse enthalten.";
+$lang['valid_emails']			= "Das Feld %s kann nur g&uuml;ltige E-Mail-Adressen enthalten.";
+$lang['valid_url']				= "Das Feld %s muss eine g&uuml;ltige Web-Adresse (URL) enthalten.";
+$lang['valid_ip']				= "Das Feld %s muss eine g&uuml;ltige IP enthalten.";
+$lang['min_length']				= "Das Feld %s muss mindestens %s Zeichen lang sein.";
+$lang['max_length']				= "Das Feld %s darf nicht l&auml;nger als %s Zeichen lang sein.";
+$lang['exact_length']			= "Das Feld %s muss genau %s Zeichen lang sein.";
+$lang['alpha']					= "Das Feld %s darf nur Buchstaben enthalten.";
+$lang['alpha_numeric']			= "Das Feld %s darf nur Buchstaben und/oder Zahlen enthalten.";
+$lang['alpha_dash']				= "Das Feld %s darf nur Buchstaben, Zahlen, Bindestriche und Unterstriche enthalten.";
+$lang['numeric']				= "Das Feld %s darf nur Zahlen enthalten.";
+$lang['is_numeric']				= "Das Feld %s darf nur zahlen&auml;hnliche Zeichen enthalten.";
+$lang['integer']				= "Das Feld %s darf nur ganze Zahlen enthalten.";
+$lang['regex_match']			= "Das Feld %s ist nicht im richtigen Format.";
+$lang['matches']				= "Das Feld %s muss mit dem Feld %s &uuml;bereinstimmen.";
+$lang['is_unique'] 				= "Das Feld %s muss einen einzigartigen Wert enthalten.";
+$lang['is_natural']				= "Das Feld %s darf nur positive Zahlen enthalten.";
+$lang['is_natural_no_zero']		= "Das Feld %s darf nur Zahlen gr&ouml;sser als Null enthalten.";
+$lang['decimal']				= "Das Feld %s muss eine Dezimalzahl enthalten.";
+$lang['less_than']				= "Das Feld %s muss eine Zahl kleiner als %s enthalten.";
+$lang['less_than_equal_to']		= "Das Feld %s muss eine Zahl kleiner oder gleich als %s enthalten.";
+$lang['greater_than']			= "Das Feld %s muss eine Zahl gr&ouml;sser als %s enthalten.";
+$lang['greater_than_equal_to']	= "Das Feld %s muss eine Zahl gr&ouml;sser oder gleich als %s enthalten.";
 
 
 /* End of file form_validation_lang.php */
